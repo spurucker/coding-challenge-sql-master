32ecab0159c15a02ca16ba13885a6b95b0e4f363
spurucker <santiago.purucker@mercadolibre.com>
2021-05-01 10:07:51 -0300

    Setup

---
+++ src/main/java/exercise/Table.java
@@ -0,0 +1,7 @@
+package exercise;
+
+/**
+ * TODO implement
+ */
+public class Table {
+}
---
+++ src/test/java/exercise/AnyTest.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AnyTest {
+
+    @Test
+    void nameAsYouWant() {
+    }
+
+}
